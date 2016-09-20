c3541fcf25c2dc3dd3ce562b0ec5b43ac85c1bae
mayifei35 <596685548@qq.com>
2016-09-20 14:54:45 -0600

    Add files via upload

--- DFS.java
+++ DFS.java
@@ -1,56 +1,48 @@
 import java.util.Random;
 import java.util.ArrayList;
-import java.util.Stack;
-//this takes wayyyyyyyyyy to long to run
+import java.util.LinkedList;
+import java.util.HashSet;
 
-class DFS extends SlidingPlayer {
-
-    ArrayList <SlidingMove> path;
-    int move_number=-1;
-    
-    // The constructor gets the initial board
-    public DFS(SlidingBoard _sb) {
-        super(_sb);
+class StackNode {
+    public SlidingBoard board;
+    public ArrayList<SlidingMove> path;
+    public StackNode(SlidingBoard b, ArrayList<SlidingMove> p) {
+	board = b;
+	path = p;
     }
-
-
-    //find the next move given a board state
-    public SlidingMove dfs(SlidingBoard board){
-	Stack <SlidingMove> stack= new Stack<SlidingMove>();
-	stack.addAll(board.getLegalMoves());
-	ArrayList<SlidingMove> seen=new ArrayList<>();
-	SlidingMove curr;
-	System.out.println("so far so good");
-	while(!stack.isEmpty()){
-	    System.out.println("in loop");
-	    curr =stack.pop();
-	    SlidingBoard childBoard=new SlidingBoard (board.size);
-	    System.out.println("perform a move");
-	    childBoard.setBoard(board);
-	    childBoard.doMove(curr);
-	    System.out.println("check goal");
-	    //if it is goal
-	    if (childBoard.isSolved()){
-		System.out.println("found it");
-		return curr;
-	    }
-	    else {
-		stack.addAll(childBoard.getLegalMoves());
-		//path.add(curr);
-	    }
-	    seen.add(curr);
-	}
-	System.out.println("failed");
-        return null;
-    }
-
-
-    // Perform a single move based on the current given board state
-    public SlidingMove makeMove(SlidingBoard board) {
-	//move_number++;
-	//return path.get(move_number);
-
-	return 	dfs(board); 
-    }   
 }
+class DFS extends SlidingPlayer {
+    ArrayList<SlidingMove> path;
+    int move_number = -1;
 
+    public DFS(SlidingBoard _sb) {
+	super(_sb);
+	path = dfs(_sb);
+    }
+    public ArrayList<SlidingMove> dfs(SlidingBoard board) {
+	HashSet<String> seen = new HashSet<String>();
+	LinkedList<StackNode> stack = new LinkedList<StackNode>();
+	StackNode currNode = new StackNode(board, new ArrayList<SlidingMove>());
+	while (!currNode.board.isSolved()) {
+	    ArrayList<SlidingMove> legal = currNode.board.getLegalMoves();
+	    for (SlidingMove move : legal) {
+		SlidingBoard childBoard = new SlidingBoard(currNode.board.size);
+		childBoard.setBoard(currNode.board);
+		childBoard.doMove(move);
+		if (!seen.contains(childBoard.toString())) {
+		    seen.add(childBoard.toString());
+		    ArrayList<SlidingMove> childPath = (ArrayList<SlidingMove>)currNode.path.clone();
+		    childPath.add(move);
+		    stack.add(new StackNode(childBoard, childPath));
+		}
+	    }
+	    currNode = stack.pop();
+	}
+	return currNode.path;
+    } 
+    
+    public SlidingMove makeMove(SlidingBoard board) {
+	move_number++;
+	return path.get(move_number);
+    }
+}
